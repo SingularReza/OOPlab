f86a5244074c3803eeccba4c872326378d2da6b0
Bhagam Chandan Chowdary <chandanchoudary@hotmail.com>
2019-01-25 11:07:31 +0530

    somethin

---
+++ indenting.cpp
@@ -0,0 +1,7 @@
+#include <iostteam>
+
+sing namespace std;
+
+int main() {
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 
 #include <iostream>
-#include<fstream>
+#include <fstream>
 #include <string>
 
 using namespace std;
@@ -30,7 +30,7 @@ class Arithmetic {
             //string to held the "000.." part to be appended
             string temp;
 
-            if(no) {
+            if(no>=0) {
                 while(no) {
                     temp = temp+'0';
                     no--;
@@ -48,9 +48,18 @@ class Arithmetic {
             }
             no = 0;
             cout<<output<<endl;
-            while(len1>=0) {
+            while(len1>=1) {
                 no1 = input1[len1] - '0';
                 no2 = input2[len1] - '0';
+
+                if(input1[0] == '-') {
+                    no1 = no1*-1;
+                }
+                //cout<<no1<<endl;
+                if(input2[0] == '-') {
+                    no2 = no2*-1;
+                }
+                
                 no = no1+no2+no;
                 no1 = no%10;
                 no = no/10;
@@ -63,8 +72,61 @@ class Arithmetic {
             return output;
         };
 
-        string multiply() {
+        string sub() {
+            //no adds 0 to smaller string to make their length equal
+            int no = 0, no1, no2;
+            //string length
+            int len1 = input1.length()-2;
+            int len2 = input2.length()-1;
+            no = len1-len2;
 
+            if(input2[0] != '-') {
+                input2 = "-" + input2;
+            }
+
+            //string to held the "000.." part to be appended
+            string temp;
+
+            if(no>=0) {
+                while(no) {
+                    temp = temp+'0';
+                    no--;
+                }
+                input2 = temp+input2;
+            }
+            else {
+                no = no*-1;
+                while(no) {
+                    temp = temp+'0';
+                    no--;
+                }
+                input1 = temp+input1;
+                len1 = len2;
+            }
+            no = 0;
+            cout<<output<<endl;
+            while(len1>=1) {
+                no1 = input1[len1] - '0';
+                no2 = input2[len1] - '0';
+
+                if(input1[0] == '-') {
+                    no1 = no1*-1;
+                }
+                cout<<no1<<endl;
+                if(input2[0] == '-') {
+                    no2 = no2*-1;
+                }
+                
+                no = no1+no2+no;
+                no1 = no%10;
+                no = no/10;
+                
+                //appending the 1's part of the added number to the resultant string
+                output = to_string(no1)+output;
+                len1--;
+            }
+
+            return output;
         };
 
         string factorial() {
