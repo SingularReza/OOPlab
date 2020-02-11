1054cc9c3b083da07c75e5e96234138cb1e7a982
Chandan Chowdary Bhagam <chandanchoudary@hotmail.com>
2020-02-11 09:42:46 +0530

    network lab 1

---
+++ network lab/client.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h> 
+#include <string.h>
+#include <arpa/inet.h>
+
+int main() {
+    int sockfd;
+    struct sockaddr_in serv_addr;
+    int i;
+    int connection;
+    char buffer[100];
+
+    if((sockfd=socket(AF_INET,SOCK_STREAM,0))<0)
+    {
+        printf("ERROR IN CREATING SOCKET ...\n");
+        exit(0);
+    }
+
+    printf("Client socket created successfully..\n");
+    
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serv_addr.sin_port = htons(50064);
+   
+    if((connection = connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr))) < 0)
+    {
+        printf("UNABLE TO CONNECT TO SERVER...\n");
+        exit(0);
+    }
+        
+	printf("Connected to socket successfully..\n");    
+
+    while(1) {
+        for(i = 0; i < 100; i++) {
+            buffer[i]='\0';
+        }
+
+        printf("Enter the expression : ");
+        scanf("%s", buffer);
+
+        send(sockfd, buffer, strlen(buffer)+1, 0);
+        if(buffer[0]=='-'&&buffer[1]=='1'&&buffer[2]=='\0') break;
+
+        for(i = 0; i < 100; i++) {
+            buffer[i]='\0';
+        }
+
+        recv(sockfd, buffer, 100, 0);
+        printf("Result sent by server is : %s \n",buffer);
+    }
+
+    close(sockfd);
+
+}
